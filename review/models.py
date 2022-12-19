edb91dc47f4735c12c6fcabb08273eb84a3d32bb
Ilias <anotherthecharacter@gmail.com>
2022-12-19 18:16:11 +0600

    review

---
+++ review/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from main.models import Post
+
+
+User = get_user_model()
+
+
+class Comments(models.Model):
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user_id = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+
+
+class PostLikes(models.Model):
+    user_id = models.ForeignKey(User, related_name='post_likes' , on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+
+
+class PostFavorites(models.Model):
+    user_id = models.ForeignKey(User, related_name='post_favorites' , on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, related_name='favorites', on_delete=models.CASCADE)
+
+
+class CommentsLikes(models.Model):
+    user_id = models.ForeignKey(User, related_name='comment_likes', on_delete=models.CASCADE)
+    comment_id = models.ForeignKey(Comment, related_name='likes', on_delete=models.CASCADE)
---
+++ review/views.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import actions
+from rest_framework.response import Response
+
+from .models import Comments, CommentsLikes
+from .serializers import CommentsSerializer
+
+
+User = get_user_model()
+
+
+class CommentsViewSet(ModelViewSet):
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
+
+
+    @action(['POST'], detail=True)
+    def like(self, request, pk=None):
+        user_id = request.data.get('user')
+        user = get_object_or_404(User, id=user_id)
+        comment = get_object_or_404(Comment, id=pk)
+        
+        if CommentsLikes.objects.filter(comment_id=comment, user_id=user).exists():
+            CommentsLikes.objects.filter(comment_id=comment, user_id=user).delete()
+        else:
+            CommentsLikes.objects.create(comment_id=comment, user_id=user)
+        
+        return Response(status=201)
