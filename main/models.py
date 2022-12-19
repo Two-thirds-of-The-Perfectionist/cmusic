a1022230f85b8d6c144318600efba8402c185227
Nuraiym0 <nuraiymmamytova0@gmail.com>
2022-12-19 16:32:37 +0600

    posts

---
+++ main/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model
+
+class Post(models.Moidel):
+    user_id = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    cover = models.ImageField(upload_to='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return f'{self.author.username} -> {self.body}'
+
