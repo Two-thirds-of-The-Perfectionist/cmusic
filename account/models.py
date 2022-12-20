c8f9b6ac984d2cc5d567c194ebdab3cc11bd8a45
Daniel <doroev.daniel1@gmail.com>
2022-12-20 11:42:36 +0600

    email

--- account/models.py
+++ account/models.py
@@ -17,6 +17,7 @@ class UserManager(BaseUserManager):
         user = self.model(email=email, username=username, **kwargs)
         user.set_password(password)
         user.save(using=self._db)
+        user.send_activation_code()
 
         return user
     
@@ -36,8 +37,29 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=False)
+    activation_code = models.CharField(max_length=8, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
     objects = UserManager()
+
+
+    def create_activation_code(self):
+        from django.utils.crypto import get_random_string
+
+
+        code = get_random_string(length=8, allowed_chars='qwertyuiopasdfghjklzxcvbnmQWERTYUIOASDFGHJKLZXCVBNM234567890')
+        self.activation_code = code
+        self.save()
+    
+
+    def send_activation_code(self):
+        from django.core.mail import send_mail
+
+
+        self.create_activation_code()
+        activation_link = f'http://localhost:8000/account/activate/{self.activation_code}'
+        message = f'Activate your account with a link:\n{activation_link}'
+        send_mail("Activate account", message, 'admin@admin.com', recipient_list=[self.email], fail_silently=False)
